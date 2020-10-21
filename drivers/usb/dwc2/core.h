e321bee578d7f43e014d82ec4d938dbe0d56aea3
Benson Gui <begu@synaptics.com>
2020-10-21 16:03:32 +0800

    usb: dwc2: add optional core clk
    On vs640, dwc2 needs two clks.
    ported from v5.4.
    
    Change-Id: I444c55125a9e6dead607684a7ef17e1b90fee7bf
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/264557
    Reviewed-by: Jisheng Zhang <Jisheng.Zhang@synaptics.com>
    Reviewed-by: Kaicheng Xie <Kaicheng.Xie@synaptics.com>

--- drivers/usb/dwc2/core.h
+++ drivers/usb/dwc2/core.h
@@ -1088,6 +1088,7 @@ struct dwc2_hsotg {
 	int     irq;
 	struct clk *clk;
 	struct clk *utmi_clk;
+	struct clk *core_clk;
 	struct reset_control *reset;
 	struct reset_control *reset_ecc;
 	struct reset_control *reset_sync;
