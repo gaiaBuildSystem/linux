f9ea19eb16e5908f756cbbd298abbca8c43b539a
Vishwaroop A <va@nvidia.com>
2023-09-25 16:33:16 +0000

    NVIDIA: SAUCE: spi: tegra210-quad: enable autosuspend delay
    BugLink: https://bugs.launchpad.net/bugs/2141529
    
    Add runtime auto suspend support and set this delay for each bus.
    
    Perf drop is seen due to clock switching during
    write operation. QSPI flash device write page size
    is 256 KB so clock switching was being done for every
    256KB due to pm_runtime_put(&pdev->dev) call.
    
    pm_runtime_put() function is called, the Linux kernel
    will begin to power down the device. This may involve
    disabling clocks, powering down voltage regulators, and
    releasing other resources etc.
    
    pm_runtime_set_autosuspend_delay function sets the
    autosuspend delay for a device. The autosuspend delay
    is the amount of time that the device will remain active
    after it has been idle before it is automatically suspended.
    The pm_runtime_set_autosuspend_delay() function can be used to
    reduce power consumption by automatically suspending devices
    when they are not in use.
    
    http://nvbugs/4229700
    
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    [ Based upon commit a47fbd17b7a7662dbb3a369d551f4650cb7ae0ba but
      update to use pm_runtime_dont_use_autosuspend() in the error and
      removal paths and drop DT property which has never been used. ]
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Brad Griffis <bgriffis@nvidia.com>
    Signed-off-by: Prathamesh Shete <pshete@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Abdur Rahman <abdur.rahman@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- drivers/spi/spi-tegra210-quad.c
+++ drivers/spi/spi-tegra210-quad.c
@@ -997,7 +997,8 @@ static int tegra_qspi_setup(struct spi_device *spi)
 
 	spin_unlock_irqrestore(&tqspi->lock, flags);
 
-	pm_runtime_put(tqspi->dev);
+	pm_runtime_mark_last_busy(tqspi->dev);
+	pm_runtime_put_autosuspend(tqspi->dev);
 
 	return 0;
 }
@@ -1717,6 +1718,10 @@ static int tegra_qspi_probe(struct platform_device *pdev)
 	init_completion(&tqspi->rx_dma_complete);
 	init_completion(&tqspi->xfer_completion);
 
+	/* TODO: Optimise autosuspend delay */
+	pm_runtime_set_autosuspend_delay(&pdev->dev, 3000);
+	pm_runtime_use_autosuspend(&pdev->dev);
+
 	pm_runtime_enable(&pdev->dev);
 	ret = pm_runtime_resume_and_get(&pdev->dev);
 	if (ret < 0) {
@@ -1733,7 +1738,8 @@ static int tegra_qspi_probe(struct platform_device *pdev)
 	tqspi->spi_cs_timing2 = tegra_qspi_readl(tqspi, QSPI_CS_TIMING2);
 	tqspi->def_command2_reg = tegra_qspi_readl(tqspi, QSPI_COMMAND2);
 
-	pm_runtime_put(&pdev->dev);
+	pm_runtime_mark_last_busy(&pdev->dev);
+	pm_runtime_put_autosuspend(&pdev->dev);
 
 	ret = request_threaded_irq(tqspi->irq, NULL,
 				   tegra_qspi_isr_thread, IRQF_ONESHOT,
@@ -1755,6 +1761,7 @@ static int tegra_qspi_probe(struct platform_device *pdev)
 exit_free_irq:
 	free_irq(qspi_irq, tqspi);
 exit_pm_disable:
+	pm_runtime_dont_use_autosuspend(&pdev->dev);
 	pm_runtime_force_suspend(&pdev->dev);
 	tegra_qspi_deinit_dma(tqspi);
 	return ret;
@@ -1767,6 +1774,7 @@ static void tegra_qspi_remove(struct platform_device *pdev)
 
 	spi_unregister_controller(host);
 	free_irq(tqspi->irq, tqspi);
+	pm_runtime_dont_use_autosuspend(&pdev->dev);
 	pm_runtime_force_suspend(&pdev->dev);
 	tegra_qspi_deinit_dma(tqspi);
 }
