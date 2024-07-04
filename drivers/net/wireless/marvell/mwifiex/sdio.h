419a460031fce5272e0c98d59dd26364f149eada
David Lin <yu-hao.lin@nxp.com>
2024-07-04 11:30:00 +0800

    wifi: mwifiex: add host mlme for client mode
    Add host based MLME to enable WPA3 functionalities in client mode.
    This feature required a firmware with the corresponding V2 Key API
    support. The feature (WPA3) is currently enabled and verified only
    on IW416. Also, verified no regression with change when host MLME
    is disabled.
    
    Upstream-Status: Backport [36995892c271cce5e2230bc165a06f109b117222]
    Signed-off-by: David Lin <yu-hao.lin@nxp.com>
    Reviewed-by: Francesco Dolcini <francesco.dolcini@toradex.com>
    Acked-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://patch.msgid.link/20240704033001.603419-2-yu-hao.lin@nxp.com

--- drivers/net/wireless/marvell/mwifiex/sdio.h
+++ drivers/net/wireless/marvell/mwifiex/sdio.h
@@ -259,6 +259,7 @@ struct sdio_mmc_card {
 	bool can_auto_tdls;
 	bool can_ext_scan;
 	bool fw_ready_extra_delay;
+	bool host_mlme;
 
 	struct mwifiex_sdio_mpa_tx mpa_tx;
 	struct mwifiex_sdio_mpa_rx mpa_rx;
@@ -283,6 +284,7 @@ struct mwifiex_sdio_device {
 	bool can_auto_tdls;
 	bool can_ext_scan;
 	bool fw_ready_extra_delay;
+	bool host_mlme;
 };
 
 /*
