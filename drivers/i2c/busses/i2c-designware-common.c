0dd0883684dd2a122e775c6fb69e2eda914d5cbf
Matheus Castello <matheus@castello.eng.br>
2026-03-29 23:13:54 -0300

    drivers: i2c: designware: Remove atomic check for sleep functions
    Luna downstream
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- drivers/i2c/busses/i2c-designware-common.c
+++ drivers/i2c/busses/i2c-designware-common.c
@@ -650,10 +650,7 @@ void __i2c_dw_disable(struct dw_i2c_dev *dev)
 			 * 25us) to ensure the I2C ENABLE bit is already set
 			 * as described in the DesignWare I2C databook.
 			 */
-			if (dev->atomic)
-				udelay(DIV_ROUND_CLOSEST_ULL(10 * MICRO, t->bus_freq_hz));
-			else
-				fsleep(DIV_ROUND_CLOSEST_ULL(10 * MICRO, t->bus_freq_hz));
+			fsleep(DIV_ROUND_CLOSEST_ULL(10 * MICRO, t->bus_freq_hz));
 			/* Set ENABLE bit before setting ABORT */
 			enable |= DW_IC_ENABLE_ENABLE;
 		}
@@ -682,10 +679,7 @@ void __i2c_dw_disable(struct dw_i2c_dev *dev)
 		 * transfer supported by the driver (for 400kHz this is
 		 * 25us) as described in the DesignWare I2C databook.
 		 */
-		if (dev->atomic)
-			udelay(25);
-		else
-			usleep_range(25, 250);
+		usleep_range(25, 250);
 	} while (timeout--);
 
 	dev_warn(dev->dev, "timeout in disabling adapter\n");
@@ -732,7 +726,7 @@ int i2c_dw_acquire_lock(struct dw_i2c_dev *dev)
 {
 	int ret;
 
-	if (dev->atomic || !dev->acquire_lock)
+	if (!dev->acquire_lock)
 		return 0;
 
 	ret = dev->acquire_lock();
@@ -746,7 +740,7 @@ int i2c_dw_acquire_lock(struct dw_i2c_dev *dev)
 
 void i2c_dw_release_lock(struct dw_i2c_dev *dev)
 {
-	if (!dev->atomic && dev->release_lock)
+	if (dev->release_lock)
 		dev->release_lock();
 }
 
@@ -764,9 +758,6 @@ int i2c_dw_wait_bus_not_busy(struct dw_i2c_dev *dev)
 				       20 * DW_IC_BUSY_POLL_TIMEOUT_US);
 	if (ret) {
 		dev_warn(dev->dev, "timeout waiting for bus ready\n");
-
-		if (dev->atomic)
-			return ret;
 
 		i2c_recover_bus(&dev->adapter);
 
@@ -1003,8 +994,7 @@ static int i2c_dw_runtime_suspend(struct device *device)
 		return 0;
 
 	i2c_dw_disable(dev);
-	clk_disable(dev->clk);
-	clk_disable(dev->pclk);
+	i2c_dw_prepare_clk(dev, false);
 
 	return 0;
 }
@@ -1013,8 +1003,7 @@ static int i2c_dw_suspend(struct device *device)
 {
 	struct dw_i2c_dev *dev = dev_get_drvdata(device);
 
-	if (!(dev->flags & ACCESS_NO_IRQ_SUSPEND))
-		i2c_mark_adapter_suspended(&dev->adapter);
+	i2c_mark_adapter_suspended(&dev->adapter);
 
 	return i2c_dw_runtime_suspend(device);
 }
@@ -1023,10 +1012,8 @@ static int i2c_dw_runtime_resume(struct device *device)
 {
 	struct dw_i2c_dev *dev = dev_get_drvdata(device);
 
-	if (!dev->shared_with_punit) {
-		clk_enable(dev->clk);
-		clk_enable(dev->pclk);
-	}
+	if (!dev->shared_with_punit)
+		i2c_dw_prepare_clk(dev, true);
 
 	i2c_dw_init(dev);
 
@@ -1038,8 +1025,7 @@ static int i2c_dw_resume(struct device *device)
 	struct dw_i2c_dev *dev = dev_get_drvdata(device);
 
 	i2c_dw_runtime_resume(device);
-	if (!(dev->flags & ACCESS_NO_IRQ_SUSPEND))
-		i2c_mark_adapter_resumed(&dev->adapter);
+	i2c_mark_adapter_resumed(&dev->adapter);
 
 	return 0;
 }
